1b30d5a39ff2b6a4ce008adf9ecea4a2100eccf5
Bartosz Lewandowski <89294735+Bart3kL@users.noreply.github.com>
2022-10-14 15:13:52 +0200

    init

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth/next';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+// import Head from 'next/head';
+import { useSession, signIn } from 'next-auth/react';
+
+const Home: NextPage = () => {
+  const handleGoogleLogin = async (e) => {
+    e.preventDefault();
+    await signIn('google');
+  };
+
+  return (
+    <div>
+      <button onClick={handleGoogleLogin}>Google</button>
+    </div>
+  );
+};
+
+export default Home;
